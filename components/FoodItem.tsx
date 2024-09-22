fd2bee96dd72aecfffc8694718f5d2d52f39b1de
manlikexavy <chibokaxavier@gmail.com>
2024-09-22 21:17:50 +0100

    top dishes section ui complete, added the pus and icon and minus icons including their functionalities

--- components/FoodItem.tsx
+++ components/FoodItem.tsx
@@ -2,6 +2,7 @@
 import { Rating } from "primereact/rating";
 import React, { useState } from "react";
 import { CiStar } from "react-icons/ci";
+import { FaMinus, FaPlus } from "react-icons/fa";
 import { TbStarFilled } from "react-icons/tb";
 interface FoodItemProps {
   id: string;
@@ -12,23 +13,46 @@ interface FoodItemProps {
 }
 
 const FoodItem = ({ id, name, price, description, image }: FoodItemProps) => {
-  const [value, setValue] = useState<number | undefined >(0);
+  const [value, setValue] = useState<number | undefined>(0);
+  const [count, setCount] = useState(0);
+
   return (
-    <div className=" rounded-2xl shadow-stone-300 shadow-md animate-appearance-in transition duration-500">
-      <div>
+    <div className="food-item rounded-2xl shadow-stone-300 shadow-md animate-appearance-in transition duration-500">
+      <div className="relative">
         <img src={image} alt="" className="w-full rounded-t-2xl" />
+        
+        <div className="absolute top-[200px] right-[20px] flex items-center">
+          {count === 0 ? (
+            <div className="bg-white h-8 w-8 cursor-pointer flex justify-center items-center rounded-full">
+              <FaPlus onClick={() => setCount((prev) => prev + 1)} />
+            </div>
+          ) : (
+            <div className="flex items-center bg-white p-1 rounded-3xl gap-3">
+              <div className="bg-red-300 h-8 w-8 cursor-pointer flex justify-center items-center text-red-800 rounded-full">
+                <FaMinus onClick={() => setCount((prev) => prev - 1)} />
+              </div>
+              <p>{count}</p>
+              <div className="bg-green-300 h-8 w-8 cursor-pointer flex justify-center items-center text-green-800 rounded-full">
+                <FaPlus onClick={() => setCount((prev) => prev + 1)} />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
       <div className="p-5">
-        <div>
-          <p>{name}</p>
-          {/* <img src={assets.rat} alt="" /> */}
+        <div className="flex justify-between items-center">
+          <p className="text-2xl font-semibold mb-2">{name}</p>
           <div className="flex gap-1">
-            {" "}
-            <Rating value={value} onChange={(e) => setValue(e.value ?? undefined)} cancel={false}  className="text-6xl text-blue-600 py-2"  />
+            <Rating
+              value={value}
+              onChange={(e) => setValue(e.value ?? undefined)}
+              cancel={false}
+              className="text-6xl text-blue-600 py-2"
+            />
           </div>
         </div>
-        <p>{description}</p>
-        <p>${price}</p>
+        <p className="text-gray-700">{description}</p>
+        <p className="mt-3 text-2xl text-rose-600">${price}</p>
       </div>
     </div>
   );
